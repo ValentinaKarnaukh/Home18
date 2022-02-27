9a4953640c95ae4385345e9b86940d85f46544ea
ValentinaKarnaukh <valia.karnaukh@yandex.ru>
2022-02-27 22:53:57 +0300

    non-static

---
+++ src/main/java/GamesConditions.java
@@ -0,0 +1,103 @@
+public class GamesConditions {
+    static boolean isGreenLight;
+    static int MAX_SPEED;
+
+    public GamesConditions(boolean isGreenLight, int MAX_SPEED) {
+        GamesConditions.isGreenLight = isGreenLight;
+        GamesConditions.MAX_SPEED = MAX_SPEED;
+    }
+
+    public int amountLoser(String[] namePlayers) {
+        int Loser = 0;
+        for (String text : namePlayers) {
+            String[] players = text.split(" ");
+            int speed = Integer.parseInt(players[1]);
+            if (!isGreenLight) {
+                if (MAX_SPEED < speed) {
+                    Loser = Loser + 1;
+                }
+            }
+        }
+        return Loser;
+    }
+
+    public int[] arraySpeedLoser(String[] namePlayers) {
+        int cnt = 0;
+        for (String text : namePlayers) {
+            String[] players = text.split(" ");
+            int speed = Integer.parseInt(players[1]);
+            if (!isGreenLight) {
+                if (MAX_SPEED < speed) {
+                    cnt++;
+                }
+            }
+        }
+        int[] speedsLoser = new int[cnt];
+        int i = 0;
+        for (String text : namePlayers) {
+            String[] players = text.split(" ");
+            int speed = Integer.parseInt(players[1]);
+            if (!isGreenLight) {
+                if (MAX_SPEED < speed) {
+                    speedsLoser[i] = speed;
+                    i++;
+                }
+            }
+        }
+        return speedsLoser;
+    }
+
+    public int[] arraySpeedWin(String[] namePlayers) {
+        int cnt = 0;
+        for (String text : namePlayers) {
+            String[] players = text.split(" ");
+            int speed = Integer.parseInt(players[1]);
+            if (!isGreenLight) {
+                if (MAX_SPEED >= speed) {
+                    cnt++;
+                }
+            }
+        }
+        int[] speedsWin = new int[cnt];
+        int i = 0;
+        for (String text : namePlayers) {
+            String[] players = text.split(" ");
+            int speed = Integer.parseInt(players[1]);
+            if (!isGreenLight) {
+                if (MAX_SPEED >= speed) {
+                    speedsWin[i] = speed;
+                    i++;
+                }
+            }
+        }
+        return speedsWin;
+    }
+
+
+    public String[] arrayNameWin(String[] namePlayers) {
+        int cnt = 0;
+        for (String text : namePlayers) {
+            String[] players = text.split(" ");
+            int speed = Integer.parseInt(players[1]);
+            if (!isGreenLight) {
+                if (speed < MAX_SPEED) {
+                    cnt++;
+                }
+            }
+        }
+
+        String[] names = new String[cnt];
+        int i = 0;
+        for (String text : namePlayers) {
+            String[] players = text.split(" ");
+            int speed = Integer.parseInt(players[1]);
+            if (!isGreenLight) {
+                if (speed < MAX_SPEED) {
+                    names[i] = players[0];
+                    i++;
+                }
+            }
+        }
+        return names;
+    }
+}
---
+++ src/test/java/GameConditionsTest.java
@@ -0,0 +1,40 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class GameConditionsTest {
+    @Test
+    public void losers() {
+        GamesConditions gamesConditions = new GamesConditions(false, 3);
+        String[] namePlayers = {"Tom 5", "Mike 0","John 2"};
+        int expected = 1;
+        int actual = gamesConditions.amountLoser(namePlayers);
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void arrayLosers() {
+        GamesConditions gamesConditions = new GamesConditions(false, 3);
+        String[] namePlayers = {"Tom 5", "Mike 0","John 2"};
+        int[] expected = {5};
+        int[] actual = gamesConditions.arraySpeedLoser(namePlayers);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void arrayWin() {
+        GamesConditions gamesConditions = new GamesConditions(false, 3);
+        String[] namePlayers = {"Tom 5", "Mike 0","John 2"};
+        int[] expected = {0, 2};
+        int[] actual = gamesConditions.arraySpeedWin(namePlayers);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
+    @Test
+    public void arrayNameWin () {
+        GamesConditions gamesConditions = new GamesConditions(false, 3);
+        String[] namePlayers = {"Tom 5", "Mike 0","John 2"};
+        String[] expected = {"Mike", "John"};
+        String[] actual = gamesConditions.arrayNameWin(namePlayers);
+        Assertions.assertArrayEquals(expected, actual);
+    }
+}
